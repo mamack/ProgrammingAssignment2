eafbd97d53fd9ad0bd06acb3394b1c5bf25b1e35
mamack <martinaamacker@hotmail.com>
2017-10-22 21:02:54 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function take a matrix and make the inverse matrix. Because it takes a long time if the matrix
+## is big, it makes the inverse in the cache.
 
-## Write a short comment describing this function
+## To test the functions the "mat"-matrix is created.
+mat = matrix(c(5,1,0,3,-1,2,4,0,-1), nrow=3, byrow=TRUE)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## The function set the inverse NULL if it exists and make a Matrix in an other environment, in the cache.
+makeCacheMatrix <- function(A = matrix()){
+        I <- NULL
+        set <- function(B){
+                A <<- B
+                I <<- NULL
+        }
+        get <- function() A
+        setInverse <- function(solve) I <<- solve
+        getInverse <- function() I
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function looks if there is an Inverse in the cache. Otherwise the function calculates the inverse if
+## the matrix has an inverse.
+cacheSolve <- function(A,...){
+        I <- A$getInverse()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        else {
+                data <- A$get()
+                if(ncol(data)!=nrow(data)){
+                        print("the matrix isn't square")
+                }
+                else if(det(data)==0){
+                        print("the determinant is zero")
+                }
+                else{
+                        I <- solve(data)
+                        A$setInverse(I)
+                        I
+                }
+        }
 }
+
+## With this code you can test the functions with the matrix mat. The second time "def" is called,
+## the data come from the cache.
+abc <- makeCacheMatrix(mat)
+def <- cacheSolve(abc)
+def <- cacheSolve(abc)
